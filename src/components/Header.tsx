e786601c2e0d45d2fe704f1b4fc71459a3b76db9
JavaTota <jomigone@gmail.com>
2025-03-24 10:09:58 -0500

    Refactor styling and structure: add CSS files, remove unused styles, and implement a new Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Container,
+  Navbar,
+  FormControl,
+  Nav,
+  Dropdown,
+  Badge,
+} from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <a href="">ShoppingCart</a>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 500 }}
+            placeholder="Search a product"
+            className="m-auto"
+          ></FormControl>
+        </Navbar.Text>
+        <Nav>
+          <Dropdown>
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" size="25px" />
+              <Badge>{100}</Badge>
+            </Dropdown.Toggle>
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              <span style={{ padding: 10 }}>Cart is Empty!</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
