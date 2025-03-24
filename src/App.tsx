f8a37bfdc40fd30777c61e4ffe23439fc837aa90
JavaTota <jomigone@gmail.com>
2025-03-24 10:21:54 -0500

    Update routing in App component: implement Routes for Home and Cart, remove Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import "./css/App.css";
+import Home from "./components/Home";
+import Card from "./components/Cart";
+import Cart from "./components/Cart";
 
 function App() {
-  return;
-  <BrowserRouter>
-    <Header />;
-    <div>
-      <Route path="/">
-        <Home></Home>
-      </Route>
-      <Route path="/">
-        <Card></Card>
-      </Route>
-    </div>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
